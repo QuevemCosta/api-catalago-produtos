4d80de0a985d452c5270bf275bf97308a03df13c
Quevem Costa <quevemcostadacruz@gmail.com>
2020-11-02 03:35:45 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors())//definir de onde pode acessar o servidor
+app.use(cors('*'))//definir de onde pode acessar o servidor
 app.use('/',require('./routes'))
 
 //
